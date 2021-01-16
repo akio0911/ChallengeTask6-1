a60a375c1b5d23d965ef82cdc412f39df251b9ed
松島悠人 <msm8610@matsushimayuuninnoMacBook-Air.local>
2021-01-16 11:44:00 +0900

    edit file

--- ChallengeTask6/ViewController.swift
+++ ChallengeTask6/ViewController.swift
@@ -15,22 +15,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.reset()
-
     }
-    @IBAction func didTapButton(_ sender: Any) {
 
+    @IBAction func didTapButton(_ sender: Any) {
         let value = Int(slider.value)
         print(value)
 
         if label.text == "\(value)" {
-
             self.alert(message: "当たり!\nあなたの値\(value)")
-
         } else {
-
             self.alert(message: "はずれ!\n あなたの値\(value)")
         }
-
     }
 
     private func reset() {
@@ -40,13 +35,10 @@ class ViewController: UIViewController {
     }
 
     private func alert(message: String) {
-
         let alert = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
-
         alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: { (  _) in
             self.reset()
         }))
-        present(alert, animated: true, completion: nil)
-
+        present(alert, animated: true, completion: nil)        
     }
-    }
+}
